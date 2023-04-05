648e83c0e6bad995cd8654888b2ee96e2d36f8e1
rirefat <rirefat.official@gmail.com>
2023-04-05 20:47:31 +0600

    connected with mongodb and created get api for calling services data

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
+
 
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', async (req, res)=>{
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d7wzr9v.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        const serviceCollection = client.db("carDoctor").collection("services");
+        
+        app.get('/services', async (req, res)=>{
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const result = await cursor.toArray();
+
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', async (req, res) => {
     res.send("Node server is running")
 });
 
-app.get('/services', async (req, res)=>{
+app.get('/services', async (req, res) => {
     res.send("Service Available")
 });
 
-app.listen(port, ()=>{
+app.listen(port, () => {
     console.log(`Node server is running on port ${port}`)
 });
